b5bf3ed3144fce938df58b60b1f64cc98e23b781
Simon - fantomitechno <fantomitechno@gmail.com>
2023-04-27 23:50:09 +0200

    🤡 English language

--- src/main/java/dev/renoux/emotes/mixins/ServerPlayerMixin.java
+++ src/main/java/dev/renoux/emotes/mixins/ServerPlayerMixin.java
@@ -33,7 +33,7 @@ public abstract class ServerPlayerMixin {
   // Messages from the server (/chat)
   @Inject(method = "sendMessage(Lnet/minecraft/text/Text;)V", at = @At("HEAD"), cancellable = true)
   private void onSendMessage(Text message, CallbackInfo ci) {
-    if (message.getString().startWith("message.voicechat"))
+    if (message.getString().startsWith("message.voicechat"))
         return;
     this.sendMessage(Emotes.processMessage(message.getString(), message.getStyle()), MessageType.SYSTEM);
     ci.cancel();
