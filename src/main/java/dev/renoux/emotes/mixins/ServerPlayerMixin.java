ceb03873bc3ad23c407fc20936a34c0d8e1c0d87
Simon - fantomitechno <fantomitechno@gmail.com>
2023-04-27 23:45:05 +0200

    🤡 Didn't maintain long enough the X on ly keyboard

--- src/main/java/dev/renoux/emotes/mixins/ServerPlayerMixin.java
+++ src/main/java/dev/renoux/emotes/mixins/ServerPlayerMixin.java
@@ -33,7 +33,7 @@ public abstract class ServerPlayerMixin {
   // Messages from the server (/chat)
   @Inject(method = "sendMessage(Lnet/minecraft/text/Text;)V", at = @At("HEAD"), cancellable = true)
   private void onSendMessage(Text message, CallbackInfo ci) {
-    if (message.getString().startWith("message.voicechatx))
+    if (message.getString().startWith("message.voicechat"))
         return;
     this.sendMessage(Emotes.processMessage(message.getString(), message.getStyle()), MessageType.SYSTEM);
     ci.cancel();
